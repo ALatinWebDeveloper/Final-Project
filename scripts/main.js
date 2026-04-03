3afeb8cab8d4083a0a710a019f8e39b4cecb3fd9
Marcus <juandbeta@gmail.com>
2026-04-03 09:09:08 +0200

    APi connected, main page is dinamic

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
-import { renderPokeCard } from "./utils.js";
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-renderPokeCard(data);
+
+for (let i = 1; i <= 151; i++) {
+        fetch(apiUrl + i)
+            .then((response) => response.json())
+            .then(data => renderPokeCard(data))
+            .catch(error => console.error("Error fetching data:", error));
+    }
+
+export function renderPokeCard(pokemon) {
+    const pokedex = document.getElementById("pokedex");
+    const card = document.createElement("div");
+
+    let type = pokemon.types.map(type =>
+        `<p class="pkm_type ${type.type.name}">${(type.type.name).charAt(0).toUpperCase() + (type.type.name).slice(1)}</p>`);
+    type = type.join('');
+
+    card.classList.add("pokemon_card");
+    card.innerHTML = `
+            <img class="favorite-icon" src="./images/icons8-favorite-48.png" alt="Favorite">
+            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
+            <p class="id">#${pokemon.id}</p>
+            <h2 class="pkm_name">${(pokemon.name).charAt(0).toUpperCase() + (pokemon.name).slice(1)}</h2>
+            ${type}
+    `;
+    pokedex.appendChild(card);
+}
+
+renderPokeCard();
---
+++ scripts/utils.mjs
@@ -0,0 +1,32 @@
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+
+    for (let i = 1; i <= 151; i++) {
+        fetch(apiUrl + i)
+            .then((response) => response.json())
+            .then(data => renderPokeCard(data))
+            .catch(error => console.error("Error fetching data:", error));
+    }
+
+export function renderPokeCard(pokemon) {
+    let tipo = pokemon.types;
+    console.log(tipo);
+
+    const pokedex = document.getElementById("pokedex");
+    const card = document.createElement("div");
+
+    card.classList.add("pokemon_card");
+    card.innerHTML = `
+        <div class="pokemon_card">
+            <a href=#"><img class="favorite-icon" src="./images/icons8-favorite-48.png" alt="Favorite"></a>
+            <img src="${pokemon.sprites.front_default}" alt="${poke.name}">
+            <p class="id">#${poke.id}</p>
+            <h2 class="pkm_name">${poke.name}</h2>
+            <p class="pkm_type grass">${poke.type}</p>
+            <p class="pkm_type poison">Poison</p>
+        </div>
+    `;
+    pokedex.appendChild(card);
+}
+
+renderPokeCard();
